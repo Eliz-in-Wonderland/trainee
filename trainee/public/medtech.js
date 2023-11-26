031ec0fb009e2cc8b9068f32bf7ee76f6a704209
Eliz-in-Wonderland <113202606+Eliz-in-Wonderland@users.noreply.github.com>
2023-11-27 00:10:03 +0300

    Add files via upload

---
+++ trainee/public/medtech.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $('.day').click(function(){
+        $(this).css('background-color', '#C2E7C8');
+        $('.week').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.two_weeks').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.three_month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.half_of_a_year').css('background-color', ' rgba(199, 199, 199, 0.538)');
+    });
+
+    $('.week').click(function(){
+        $(this).css('background-color', '#C2E7C8');
+        $('.day').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.two_weeks').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.three_month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.half_of_a_year').css('background-color', ' rgba(199, 199, 199, 0.538)');
+    });
+
+    $('.two_weeks').click(function(){
+        $(this).css('background-color', '#C2E7C8');
+        $('.day').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.week').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.three_month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.half_of_a_year').css('background-color', ' rgba(199, 199, 199, 0.538)');
+    });
+
+    $('.month').click(function(){
+        $(this).css('background-color', '#C2E7C8');
+        $('.day').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.two_weeks').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.week').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.three_month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.half_of_a_year').css('background-color', ' rgba(199, 199, 199, 0.538)');
+    });
+
+    $('.three_month').click(function(){
+        $(this).css('background-color', '#C2E7C8');
+        $('.day').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.two_weeks').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.week').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.half_of_a_year').css('background-color', ' rgba(199, 199, 199, 0.538)');
+    });
+
+    $('.half_of_a_year').click(function(){
+        $(this).css('background-color', '#C2E7C8');
+        $('.day').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.two_weeks').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.three_month').css('background-color', ' rgba(199, 199, 199, 0.538)');
+        $('.week').css('background-color', ' rgba(199, 199, 199, 0.538)');
+    });
+
+
+});
